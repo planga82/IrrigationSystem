87fa27634b5e60d8b8aaa06cef8f95135d0873b8
Pablo Langa <pablo.langa@stratio.com>
2023-02-11 17:10:11 -0300

    Fix rotation

--- src/logger.py
+++ src/logger.py
@@ -10,14 +10,18 @@ def initialice(base_folder = "logs"):
 def rotate_file(base_folder = "logs", base_file_name = "out.log", num_max_files = 5):
     dirs = os.listdir(base_folder)
     file_base = base_folder + "/" + base_file_name
-    if len(dirs) >= num_max_files:
+    num_files = len(dirs)
+    if num_files >= num_max_files:
         os.remove(file_base + str(num_max_files))
-    for i in range(num_max_files, 1, -1):
-        os.rename(file_base + str(i-1), file_base + str(i))
+        num_files = num_files - 1
+    for i in range(num_files + 1, 1, -1):
+        os.rename(file_base + str(i - 1), file_base + str(i))
     open(file_base + str(1), 'a').close()   
         
 def write_log_line(line, base_folder = "logs", base_file_name = "out.log", max_bytes_file = 1000):
+    initialice()
     file_base = base_folder + "/" + base_file_name
+    open(file_base + str(1), 'a').close()
     byte_size = os.stat(file_base + str(1))[6]
     if (byte_size > max_bytes_file):
         rotate_file()
@@ -26,3 +30,5 @@ def write_log_line(line, base_folder = "logs", base_file_name = "out.log", max_b
     file.flush()
     file.close()
     
+for x in range(210):
+    write_log_line("0123456789")
