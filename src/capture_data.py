75bb2bc1061a1de97c8f8a5c2ad5b96536bbeb09
Pablo Langa <pablo.langa@stratio.com>
2023-02-11 19:41:30 -0300

    capture data

---
+++ src/capture_data.py
@@ -0,0 +1,19 @@
+import machine
+import utime
+   
+    
+def capture_temperature():
+    sensor_temp = machine.ADC(4)
+    conversion_factor = 3.3 / 65535
+    reading = sensor_temp.read_u16() * conversion_factor
+    temperature = 27 - (reading - 0.706)/0.001721
+    return str(round(temperature, 2))
+
+def capture_soil_moisture():
+    soil = machine.ADC(machine.Pin(26))
+    min_moisture=0
+    max_moisture=65535
+    moisture = (max_moisture - soil.read_u16()) * 100 / (max_moisture - min_moisture)
+    return str(int(moisture))
+
+    
