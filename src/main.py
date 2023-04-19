612682dbedeb2722bd0c64fde72d9f7410bb4d21
Pablo Langa <pablo.langa@stratio.com>
2023-04-19 16:26:52 -0400

    fix multi-thread memory problems

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import _thread
 import utime
+import gc
 
 execfile("utils.py")
 execfile("logger.py")
@@ -8,41 +9,24 @@ execfile("water_pump_control.py")
 execfile("machine_state.py")
 
 
-
-def thread1():
-    try:
-        counter1 = 1
-        while True:
-            counter1 = update_counter(counter1)
-            #--------------------------------------
-            if(led_state == 1):
-                execute_every(counter1, 1, turn_on_internal_led, 0.3)
-            else:
-                execute_every(counter1, 5, turn_on_internal_led, 1)
-            #--------------------------------------
-            utime.sleep(0.3)
-    except (OSError, Exception) as error:
-        write_log_line([str(error)])
-        
-    
-    
-def thread2():
-    try:
-        counter2 = 1
-        while True:
-            counter2 = update_counter(counter2)
-            #--------------------------------------
-            execute_every(counter2, 2, state_transitions)
-            execute_every(counter2, 2, print_current_status, current_state, current_cycles, button_engine_state, button_mode_state, engine_state, total_irrigation_cicles, total_waiting_cycles)
-            execute_every(counter2, 1, engine_button)
-            execute_every(counter2, 1, mode_button)
-            execute_every(counter2, 2, update_engine_state, engine_state)
-            #--------------------------------------
-            utime.sleep(0.5)
-    except (OSError, Exception) as error:
-        write_log_line([str(error)])
-        
-_thread.start_new_thread(thread1, [])
-thread2()
+try:
+    counter2 = 1
+    while True:
+        counter2 = update_counter(counter2)
+        #--------------------------------------
+        execute_every(counter2, 4, state_transitions)
+        execute_every(counter2, 4, print_current_status, current_state, current_cycles, button_engine_state, button_mode_state, engine_state, total_irrigation_cicles, total_waiting_cycles)
+        execute_every(counter2, 2, engine_button)
+        execute_every(counter2, 2, mode_button)
+        execute_every(counter2, 4, update_engine_state, engine_state)
+        if(led_state == 1):
+            execute_every(counter2, 1, turn_internal_led)
+        else:
+            execute_every(counter2, 5, turn_internal_led)
+        #--------------------------------------
+        utime.sleep(0.25)        
+        gc.collect()
+except (OSError, Exception) as error:
+    write_log_line([str(error)])
 
     
